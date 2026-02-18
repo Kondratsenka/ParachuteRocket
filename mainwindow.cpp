243d5c0d7e3b729d06ead52009eaa229e777c1ac
Slava <viavhaslaukandratsenka@gmail.com>
2026-02-19 00:06:10 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,16 +13,3 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-struct Rocket {
-    int altitude;                                    // высота
-    float velocity;                                 // скорость
-    float mass;                                    // масса
-    float fuel;                                   // количество топлива
-    int thrust;                                  // сила двигателя
-};
-
-struct Parachute {
-
-    float deployAltitude;                   //высота раскрытия 
-    float drag;                            // сила сопратевления
-};
