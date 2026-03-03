f56d2fced953cf3247cc1cbcc8718bb9a511e635
Slava <viavhaslaukandratsenka@gmail.com>
2026-03-03 23:54:08 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -15,7 +15,7 @@ struct Rocket {
 
 struct Parachute {
 
-    float deployAltitude;                  // высота раскрытия
+    float deployAltitude;                  // высота раскрытия парашюта
     float drag;                           // сила сопратевления
 
 };
